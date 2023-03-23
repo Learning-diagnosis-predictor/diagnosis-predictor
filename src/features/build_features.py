50bbeeb738925180cdc9e65c9579f50b85acdc61
charlie42 <oninfrared@gmail.com>
2023-03-23 15:54:19 +0100

    add processing speed diag

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -10,6 +10,7 @@ def make_new_diag_cols(data, diag_cols):
     data["New Diag.Intellectual Disability-Mild"] = (data["WISC,WISC_FSIQ"] < 70) | (data["Diag.Borderline Intellectual Functioning"] == 1)
     data["New Diag.Borderline Intellectual Functioning"] = ((data["WISC,WISC_FSIQ"] < 85) & (data["WISC,WISC_FSIQ"] > 70)) | (data["Diag.Intellectual Disability-Mild"] == 1)
     data["New Diag.Specific Learning Disorder with Impairment in Written Expression"] = (data["Diag.Specific Learning Disorder with Impairment in Written Expression"] == 1) # No task for written expression
+    data["New Diag.Processing Speed Deficit"] = (data["WISC,WISC_PSI"] < 85) 
 
     print("New diagnosis columns positive value counts:")
     print(data["New Diag.Specific Learning Disorder with Impairment in Reading"].value_counts())
@@ -17,5 +18,6 @@ def make_new_diag_cols(data, diag_cols):
     print(data["New Diag.Specific Learning Disorder with Impairment in Mathematics"].value_counts())
     print(data["New Diag.Intellectual Disability-Mild"].value_counts())
     print(data["New Diag.Borderline Intellectual Functioning"].value_counts())
+    print(data["New Diag.Processing Speed Deficit"].value_counts())
 
     return data
