6d3622846cd42830d255e25344b179d190130b8b
charlie42 <oninfrared@gmail.com>
2023-03-24 10:56:57 +0100

    save lr coefs to reports

--- src/models/helpers/__init__.py
+++ src/models/helpers/__init__.py
@@ -3,6 +3,10 @@ from helpers.get_feature_subsets_from_sfs import *
 from helpers.idenitfy_thresholds import *
 from helpers.file_helpers import *
 from helpers.lr_coefficients_helpers import *
+<<<<<<< HEAD
 from helpers.get_performance_on_feature_subsets import get_performances_on_feature_subsets
 from helpers.re_train_models_on_subsets import re_train_models_on_feature_subsets, get_top_n_features
 from helpers.write_feature_subsets_to_file import write_feature_subsets_to_file
+=======
+from helpers.get_performance_on_feature_subsets import get_performances_on_feature_subsets
+>>>>>>> 6ead447 (save lr coefs to reports)
