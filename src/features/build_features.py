7f56a509ff3e00e85ee3df291f30510345401e07
charlie42 <oninfrared@gmail.com>
2023-06-02 13:32:32 -0400

    new test based rules

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,19 +1,19 @@
 def make_new_diag_cols(data):
 
-    # # Create new diganosis columns: positive if consensus diagnosis is positive OR if WIAT or WISC score is within range
-    # data["New Diag.Specific Learning Disorder with Impairment in Reading"] = data["WIAT,WIAT_Word_Stnd"] < 85
-    # data["New Diag.Specific Learning Disorder with Impairment in Mathematics"] = data["WIAT,WIAT_Num_Stnd"] < 85
-    # data["New Diag.Intellectual Disability-Mild"] = data["WISC,WISC_FSIQ"] < 70
-    # data["New Diag.Borderline Intellectual Functioning"] = (data["WISC,WISC_FSIQ"] < 85) & (data["WISC,WISC_FSIQ"] > 70) 
-    # data["New Diag.Specific Learning Disorder with Impairment in Written Expression"] = (data["Diag.Specific Learning Disorder with Impairment in Written Expression"] == 1) # No task for written expression
-    # data["New Diag.Processing Speed Deficit"] = (data["WISC,WISC_PSI"] < 85) 
-
-    data["New Diag.Specific Learning Disorder with Impairment in Reading"] = data["Diag.Specific Learning Disorder with Impairment in Reading"]
-    data["New Diag.Specific Learning Disorder with Impairment in Mathematics"] = data["Diag.Specific Learning Disorder with Impairment in Mathematics"]
-    data["New Diag.Intellectual Disability-Mild"] = data["Diag.Intellectual Disability-Mild"]
-    data["New Diag.Borderline Intellectual Functioning"] = data["Diag.Borderline Intellectual Functioning"]
-    data["New Diag.Specific Learning Disorder with Impairment in Written Expression"] = data["Diag.Specific Learning Disorder with Impairment in Written Expression"] == 1 # No task for written expression
+    # Create new diganosis columns: positive if consensus diagnosis is positive OR if WIAT or WISC score is within range
+    data["New Diag.Specific Learning Disorder with Impairment in Reading"] = (data["WIAT,WIAT_Word_Stnd"] < 85) & (data["WISC,WISC_FSIQ"] > 70)
+    data["New Diag.Specific Learning Disorder with Impairment in Mathematics"] = (data["WIAT,WIAT_Num_Stnd"] < 85) & (data["WISC,WISC_FSIQ"] > 70)
+    data["New Diag.Specific Learning Disorder with Impairment in Written Expression"] = (data["WIAT,WIAT_Spell_Stnd"] < 85) & (data["WISC,WISC_FSIQ"] > 70)
+    data["New Diag.Intellectual Disability-Mild"] = data["WISC,WISC_FSIQ"] < 70
+    data["New Diag.Borderline Intellectual Functioning"] = (data["WISC,WISC_FSIQ"] < 85) & (data["WISC,WISC_FSIQ"] > 70) 
     data["New Diag.Processing Speed Deficit"] = (data["WISC,WISC_PSI"] < 85) 
+
+    # data["New Diag.Specific Learning Disorder with Impairment in Reading"] = data["Diag.Specific Learning Disorder with Impairment in Reading"]
+    # data["New Diag.Specific Learning Disorder with Impairment in Mathematics"] = data["Diag.Specific Learning Disorder with Impairment in Mathematics"]
+    # data["New Diag.Intellectual Disability-Mild"] = data["Diag.Intellectual Disability-Mild"]
+    # data["New Diag.Borderline Intellectual Functioning"] = data["Diag.Borderline Intellectual Functioning"]
+    # data["New Diag.Specific Learning Disorder with Impairment in Written Expression"] = data["Diag.Specific Learning Disorder with Impairment in Written Expression"] == 1 
+    # data["New Diag.Processing Speed Deficit"] = (data["WISC,WISC_PSI"] < 85) 
 
     print("New diagnosis columns positive value counts:")
     print(data["New Diag.Specific Learning Disorder with Impairment in Reading"].value_counts())
