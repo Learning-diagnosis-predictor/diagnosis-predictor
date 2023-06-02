bbd7de62f6e83d90368ece2c088c2718a0de7628
charlie42 <oninfrared@gmail.com>
2023-06-02 13:12:16 -0400

    add iq to sld and add wiat task for writing

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,11 +1,13 @@
 def make_new_diag_cols(data):
 
     # Create new diganosis columns: positive if consensus diagnosis is positive OR if WIAT or WISC score is within range
-    data["New Diag.Specific Learning Disorder with Impairment in Reading"] = (data["WIAT,WIAT_Word_Stnd"] < 85) 
-    data["New Diag.Specific Learning Disorder with Impairment in Mathematics"] = (data["WIAT,WIAT_Num_Stnd"] < 85)
+    data["New Diag.Specific Learning Disorder with Impairment in Reading"] = (data["WIAT,WIAT_Word_Stnd"] < 85) & (data["WISC,WISC_FSIQ"] > 70)
+    data["New Diag.Specific Learning Disorder with Impairment in Mathematics"] = (data["WIAT,WIAT_Num_Stnd"] < 85) & (data["WISC,WISC_FSIQ"] > 70)
+    data["New Diag.Specific Learning Disorder with Impairment in Written Expression"] = (data["WIAT,WIAT_Spell_Stnd"] < 85)  & (data["WISC,WISC_FSIQ"] > 70)
     data["New Diag.Intellectual Disability-Mild"] = (data["WISC,WISC_FSIQ"] < 70) 
     data["New Diag.Borderline Intellectual Functioning"] = ((data["WISC,WISC_FSIQ"] < 85) & (data["WISC,WISC_FSIQ"] > 70))
     data["New Diag.Processing Speed Deficit"] = (data["WISC,WISC_PSI"] < 85) 
+    
 
     data["New Diag.Specific Learning Disorder with Impairment in Reading - Consensus"] = (data["Diag.Specific Learning Disorder with Impairment in Reading"] == 1)
     data["New Diag.Specific Learning Disorder with Impairment in Mathematics - Consensus"] = (data["Diag.Specific Learning Disorder with Impairment in Mathematics"] == 1)
@@ -17,6 +19,7 @@ def make_new_diag_cols(data):
     print("New diagnosis columns positive value counts:")
     print(data["New Diag.Specific Learning Disorder with Impairment in Reading"].value_counts())
     print(data["New Diag.Specific Learning Disorder with Impairment in Mathematics"].value_counts())
+    print(data["New Diag.Specific Learning Disorder with Impairment in Written Expression"].value_counts())
     print(data["New Diag.Intellectual Disability-Mild"].value_counts())
     print(data["New Diag.Borderline Intellectual Functioning"].value_counts())
     print(data["New Diag.Processing Speed Deficit"].value_counts())
